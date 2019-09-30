bd37b7d4a8da0fd60d8b3e0e3966365df054dc18
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-30 15:29:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,14 +21,12 @@ def get_japanese_emoticon(file_path, western_emo)
   emo_hash = load_library(file_path)
   
   if !emo_hash[western_emo]
+    puts "Sorry I don't know that emoji"
+    return nil
+  end
   jap_emo = emo_hash["get_emoticon"][western_emo]
   
-  if !jap_emo
-    puts "Sorry, I don't know what that emoji means"
-    return nil
-  else
-    return jap_emo
-  end
+  return jap_emo
 end
 
 def get_english_meaning(file_path, western_emo)
